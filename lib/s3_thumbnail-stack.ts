a6a332177522146df37ba84ba2fbfce42a9b1bdb
Ryan <dsouza.ryan.11@gmail.com>
2020-09-11 16:39:04 +0530

    basic setup

--- lib/s3_thumbnail-stack.ts
+++ lib/s3_thumbnail-stack.ts
@@ -1,8 +1,8 @@
-import * as cdk from '@aws-cdk/core';
+import * as cdk from '@aws-cdk/core'
 
 export class S3ThumbnailStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    super(scope, id, props)
 
     // The code that defines your stack goes here
   }
